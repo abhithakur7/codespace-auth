916dfe2629ab133474cec8b4e6ba9f55eb2e8fad
Abhishek Thakur <75234390+abhithakur7@users.noreply.github.com>
2024-02-18 07:37:58 +0000

    updated package.json

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const app = require("./app")
+const { PORT } = process.env
 
-app.listen(process.env.PORT, () => console.log(`Server is running at ${process.env.PORT}`))
+app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
